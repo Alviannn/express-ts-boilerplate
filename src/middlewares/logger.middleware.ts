accc208676daad7662e9cee94560118997a22f80
Alviannn <alvianndq@gmail.com>
2022-04-16 17:13:25 +0700

    Separate stream options

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,18 +1,22 @@
 import morgan from 'morgan';
 import logger from '../utils/logger.util';
 
+import type { StreamOptions } from 'morgan';
+
 const LOGGING_FORMAT =
     '":method :url" :status - ' +
     ':response-time ms ":user-agent"';
 
+const streamOptions: StreamOptions = {
+    write(str) {
+        logger.info(str.trim());
+    }
+};
+
 const handleLogging = morgan(
     LOGGING_FORMAT,
     {
-        stream: {
-            write(str) {
-                logger.info(str.trim());
-            }
-        }
+        stream: streamOptions
     }
 );
 
