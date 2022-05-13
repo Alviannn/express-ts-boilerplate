f59ebd000e8b7d6138eb0034c7966fc09fd0fb86
Alviannn <alvianndq@gmail.com>
2022-05-13 20:55:10 +0700

    Change auth service to accept tokens from both header and cookies

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -7,13 +7,13 @@ import { RefreshToken } from '../database/entities/refresh-token.entity';
 import { User } from '../database/entities/user.entity';
 import { ResponseError } from '../utils/api.util';
 
-import type { UserPayload, TokenType } from '../typings/auth';
+import type { UserPayload, TokenType, AuthTokens } from '../typings/auth';
 import type { Request } from 'express';
 import type { LoginType, RegisterType } from '../validations/user.validation';
 
 class AuthService {
 
-    async login({ email, password }: LoginType) {
+    async login({ email, password }: LoginType): Promise<AuthTokens> {
         const foundUser = await User.findOneBy({ email });
         if (!foundUser) {
             throw new ResponseError(
@@ -87,38 +87,44 @@ class AuthService {
     }
 
     /**
-     * Extracts the from the JWT within the Authorization header.
+     * Extracts a JWT token from the request.
      *
-     * With this we can get the raw token without manual extraction,
-     * although for getting the user payload, you might need
-     * {@link getPayloadFromHeader} function.
+     * Depending on the {@link TokenType}, the method of retrieving the
+     * token differs since the `refreshToken` uses httpOnly cookies and
+     * the `accessToken` uses the 'Bearer' authorization header.
      *
-     * This function **doesn't verify the token**.
+     * NOTE: This function **doesn't verify the JWT token**.
      */
-    getTokenFromHeader(req: Request) {
-        const rawToken = req.header('authorization');
-        const prefix = 'Bearer ';
+    getTokenFromRequest(req: Request, type: TokenType) {
+        let token: string;
 
-        if (!rawToken || !rawToken.startsWith(prefix)) {
-            return;
+        if (type === 'ACCESS') {
+            const rawToken = req.header('authorization');
+            const prefix = 'Bearer ';
+
+            if (!rawToken || !rawToken.startsWith(prefix)) {
+                return;
+            }
+
+            token = rawToken.replace(prefix, '');
+        } else {
+            token = req.cookies.refreshToken;
         }
 
-        const token = rawToken.replace(prefix, '');
         return token;
     }
 
     /**
-     * Extracts the user data payload from the JWT
-     * within the Authorization header.
-     *
+     * Extracts a user data (payload) from a JWT token.
      * Before it extracts the payload, it'll verify the token first.
      *
+     * @see {@link getTokenFromRequest}
      * @see `authenticate` middleware
      */
-    async getPayloadFromHeader(
+    async getPayloadFromRequest(
         req: Request, tokenType: TokenType = 'ACCESS') {
 
-        const token = this.getTokenFromHeader(req);
+        const token = this.getTokenFromRequest(req, tokenType);
         if (!token) {
             return;
         }
