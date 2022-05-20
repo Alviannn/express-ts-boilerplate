a32cddaa2d97a844a32d9458b1d9a5b40b16889c
Alviannn <alvianndq@gmail.com>
2022-05-20 21:42:39 +0700

    Fix datasource path

--- scripts/index.ts
+++ scripts/index.ts
@@ -8,6 +8,6 @@ export function executeCommand(command: string, args?: string[]) {
     });
 }
 
-export const DATASOURCE_PATH = './src/database/ormconfig.ts';
+export const DATASOURCE_PATH = './src/database/datasource.ts';
 export const MIGRATIONS_DIR_PATH = './src/database/migrations';
 export const ORM_CMD = 'yarn typeorm';
